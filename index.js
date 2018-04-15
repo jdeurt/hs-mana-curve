421028bda99042ced318b3793ce29a9a823d1aaf
Sharky! <jdeurt@gmail.com>
2018-04-15 16:25:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = (costs = [], max = 8) => {
+    if(costs.length > 8) {
+        for(var i = 8; i < costs.length; i++) {
+            costs[7] += costs[i];
+        }
+        costs = costs.slice(0, 8);
+    }
+    var curve = "";
+    for(var layer = 0; layer < max; layer++) {
+        costs.forEach(cost => {
+            if(cost > max-1-layer) curve += "\xDB ";
+            else if(cost == 0 && layer == max-1) curve += "_ ";
+            else curve += "  ";
+        });
+        curve += "\n";
+    }
+    curve += "0 1 2 3 4 5 6 7+";
+    return curve;
+};
